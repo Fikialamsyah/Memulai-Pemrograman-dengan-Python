a3a77e6c5fcabb68a537ad63392bf4693e14cba1
fikialamsyah <fikialamsyah@github.com>
2020-04-06 22:19:04 +0700

    Menambahkan folder latihan 01-Input Output

---
+++ 01 - Input Output/Input.py
@@ -0,0 +1,3 @@
+# input
+num = input('Enter a number: ')
+
---
+++ 01 - Input Output/Output.py
@@ -0,0 +1,26 @@
+# Print
+print('Hello, World!')
+
+# Memasukan nilai variabel pada string
+a = 5
+print('The value of a is', a)
+
+# Menampilkan text(string) dengan menggunakan format
+print('hello {}'.format('Kaka'))
+
+# Menambahkan string dan integer
+name = 'fiki'
+age = 19
+print('%s is %d years old.' % (name,age))
+
+# Menambahkan objek selain string (otomatis konversi)
+mylist = [1,2,3]
+print('A list : %s' % mylist)
+
+# Mencetak representasi Hexa (bilangan baris 16)
+a, b = 10,11
+a, b
+print('a: %x and b: %X' % (a,b))
+
+
+
